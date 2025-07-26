48988e4b212ede2f91af5114b854ecb2115a68cc
V-sahil1 <sahilvardekar1@gmail.com>
2025-07-26 19:09:35 +0530

    image

---
+++ consent/index.js
@@ -0,0 +1,37 @@
+const images=[
+    {
+        images :'/image/html.png'
+    },
+    {
+        images :'/image/css.png'
+    },
+    {
+        images :'/image/js.png'
+    },
+    {
+        images :'/image/react.png'
+    },
+    {
+        images :'/image/tailwind.png'
+    },
+    {
+        images :'/image/nodejs.png'
+    },
+    {
+        images :'/image/express.png'
+    },
+    {
+        images :'/image/mongodb.png'
+    },
+    {
+        images :'/image/postman.png'
+    },
+    {
+        images :'/image/gsapr.png'
+    },
+    {
+        images :'/image/c++.png'
+    },
+]
+
+export default images
