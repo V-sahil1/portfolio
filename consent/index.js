94e6fd6c37ca8fd5508726e384b0c039b28aab53
V-sahil1 <sahilvardekar1@gmail.com>
2025-07-27 14:30:40 +0530

    Images

--- consent/index.js
+++ consent/index.js
@@ -1,37 +1,155 @@
-const images=[
-    {
-        images :'/image/html.png'
-    },
-    {
-        images :'/image/css.png'
-    },
-    {
-        images :'/image/js.png'
-    },
-    {
-        images :'/image/react.png'
-    },
-    {
-        images :'/image/tailwind.png'
-    },
-    {
-        images :'/image/nodejs.png'
-    },
-    {
-        images :'/image/express.png'
-    },
-    {
-        images :'/image/mongodb.png'
-    },
-    {
-        images :'/image/postman.png'
-    },
-    {
-        images :'/image/gsapr.png'
-    },
-    {
-        images :'/image/c++.png'
-    },
-]
+const images = [
+  {
+    images: "/image/html.png",
+  },
+  {
+    images: "/image/css.png",
+  },
+  {
+    images: "/image/js.png",
+  },
+  {
+    images: "/image/react.png",
+  },
+  {
+    images: "/image/tailwind.png",
+  },
+  {
+    images: "/image/nodejs.png",
+  },
+  {
+    images: "/image/express.png",
+  },
+  {
+    images: "/image/mongodb.png",
+  },
+  {
+    images: "/image/postman.png",
+  },
+  {
+    images: "/image/gsapr.png",
+  },
+  {
+    images: "/image/c++.png",
+  },
+];
 
-export default images
+export const project = [
+  {
+    id: 1,
+    name: "The Gentle Pour",
+    img: "/image/mojito.png",
+    description:
+      "An animated cocktail website built with ReactJS and GSAP, featuring scroll-based animations, a dark luxury theme, and a responsive interactive UI",
+    github: "https://github.com/V-sahil1/mojito",
+    vercel: "https://mojito-n6yi.vercel.app/",
+    techStack: [
+      "/image/react.png",
+      "/image/tailwind.png",
+      "/image/gsapr.png",
+      "/image/rrouter.png",
+      "/image/vercel.png",
+    ],
+  },
+  {
+    id: 2,
+    name: "OptiCare",
+    img: "/image/opticare.png",
+    description:
+      "A healthcare appointment booking platform built with ReactJS, featuring real-time slot booking, Google/GitHub login, profile management, and QR-based UPI payments.",
+    github: "https://github.com/V-sahil1/opti-care",
+    vercel: "https://opti-care-zeta.vercel.app/",
+    techStack: [
+      "/image/js.png",
+      "/image/react.png",
+      "/image/tailwind.png",
+      "/image/clerk.png",
+      "/image/vercel.png",
+    ],
+  },
+  {
+    id: 3,
+    name: "TripNest",
+    img: "/image/Tripnest.png",
+    description:
+      "A responsive hotel booking platform built with ReactJS, featuring Google Maps integration, secure Google login via Clerk, and a static owner dashboard.",
+    github: "https://github.com/V-sahil1/Trip-nest",
+    vercel: "https://trip-nest-lfhd.vercel.app/",
+    techStack: [
+      "/image/react.png",
+      "/image/tailwind.png",
+      "/image/clerk.png",
+      "/image/map.png",
+      "/image/vercel.png",
+    ],
+  },
+  {
+    id: 4,
+    name: "AI Image Generator",
+    img: "/image/ai.png",
+    description:
+      "An AI-powered image generator built with Next.js and TypeScript, featuring text-to-image conversion, UPI QR payments, instant downloads, and a sleek animated UI.",
+    github: "https://github.com/V-sahil1/Ai-image-Generator",
+    vercel: "https://ai-image-generator-acv3.vercel.app/",
+    techStack: [
+      "/image/nextjs.png",
+      "/image/Typescript.png",
+      "/image/tailwind.png",
+      "/image/shadcn.png",
+    ],
+  },
+  {
+    id: 5,
+    name: "Tomato",
+    img: "/image/Tomato.png",
+    description:
+      "A food delivery web app built with ReactJS 18, offering food categorization, cart management, QR-based payments, Google login, and email collection using Web3Forms.",
+    github: "https://github.com/V-sahil1/Food-Delivery",
+    vercel: "https://food-delivery-jet-kappa.vercel.app/",
+    techStack: [
+      "/image/react.png",
+      "/image/tailwind.png",
+      "/image/js.png",
+      "/image/vercel.png",
+    ],
+  },
+  {
+    id: 6,
+    name: "World Atlas",
+    img: "/image/atlas.png",
+    description:
+      "A ReactJS 19 web app that allows users to search, sort, and filter countries by continent, with real-time data fetched via Axios from an external API.",
+    github: "https://github.com/V-sahil1/world-atlas",
+    vercel: "https://world-atlas-seven.vercel.app/    ",
+    techStack: [
+      "/image/react.png",
+      "/image/css.png",
+      "/image/axios.png",
+      "/image/vercel.png",
+    ],
+  },
+];
+
+export const certificates = [
+  {
+    id: 1,
+    Name: "Unlocking the Power of Javascript",
+    org: "Scaler",
+    img: "/image/scaler.png",
+    description:"Certified in JavaScript after completing a comprehensive course covering fundamentals to advanced concepts", 
+    drive: "https://drive.google.com/file/d/1iTnC351N4NkAzqthX6Nkr2fWgJOPDka4/view?usp=drive_link",
+  },
+  {
+    id: 2,
+    Name: "React Js",
+      org: "Scaler",
+    img: "/image/scaler.png",
+    description:"Certified in ReactJS with practical experience in components, state, hooks, and building real-world web apps"
+    ,
+    drive:
+      "https://drive.google.com/file/d/16Tc5cs2LJ2SicqPizWthxO6w7dC4smC0/view?usp=sharing",
+  },
+ 
+];
+
+export default images;
